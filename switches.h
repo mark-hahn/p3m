33b248684da70b69642a9aa3f264e0133f8cd6b6
Mark Hahn <mark@hahnca.com>
2018-04-12 19:14:08 -0700

    added switches.h

---
+++ switches.h
@@ -0,0 +1,11 @@
+#ifndef SWITCHES_H
+#define	SWITCHES_H
+
+#define swTopLft 0x40
+#define swBotLft 0x20
+#define swTopRgt 0x10
+#define swBotRgt 0x08
+#define swPwrOff 0x01
+
+#endif	/* SWITCHES_H */
+
